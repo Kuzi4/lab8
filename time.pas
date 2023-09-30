0110fb0b309609a7376f268d59ae26246357c44e
local user <user@mil8a-118a-11.mmcs.sfedu.ru>
2023-09-30 09:05:53 +0300

    Добавил time.pas

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  Assert(year > 0);
+  if (year mod 4 = 0) and (year mod 100 <> 0) and (year mod 400 <> 0) then
+    Print('Високосный')
+  else Print('Не високосный');
+end.
