b5635e5bd4c4063eb37c17e7360d2d61e7d9731a
local user <user@mil8a-118a-11.mmcs.sfedu.ru>
2023-09-30 09:10:09 +0300

    Обновил time.pas

--- time.pas
+++ time.pas
@@ -2,6 +2,6 @@
   var year := ReadInteger('Введите год:');
   Assert(year > 0);
   if (year mod 4 = 0) and (year mod 100 <> 0) and (year mod 400 <> 0) then
-    Print('Високосный')
-  else Print('Не високосный');
+    Print(366)
+  else Print(365);
 end.
